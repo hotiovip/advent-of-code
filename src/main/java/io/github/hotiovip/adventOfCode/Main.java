35b463b3e9817edeeb7e646a6e1a07909a18318f
Hotiovip <hotiovipita@gmail.com>
2025-09-26 23:03:43 +0200

    Historian Hysteria

---
+++ src/main/java/io/github/hotiovip/adventOfCode/Main.java
@@ -0,0 +1,9 @@
+package io.github.hotiovip.adventOfCode;
+
+import io.github.hotiovip.adventOfCode.historianHysteria.HistorianHysteria;
+
+public class Main {
+    public static void main(String[] args) {
+        HistorianHysteria historianHysteria = new HistorianHysteria();
+    }
+}
---
+++ src/main/java/io/github/hotiovip/adventOfCode/historianHysteria/HistorianHysteria.java
@@ -0,0 +1,91 @@
+package io.github.hotiovip.adventOfCode.historianHysteria;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HistorianHysteria {
+    private final Logger logger = LoggerFactory.getLogger(HistorianHysteria.class);
+    int[] leftList;
+    int[] rightList;
+
+    public HistorianHysteria() {
+        String fileData = readFile("locations.txt");
+        if (fileData == null) return;
+
+        ArrayList<int[]> splitFileData = splitFileData(fileData);
+        leftList = splitFileData.getFirst();
+        rightList = splitFileData.getLast();
+        Arrays.sort(leftList);
+        Arrays.sort(rightList);
+
+        System.out.println("Day 1: Historian Hysteria");
+        calculateDistanceBetweenLists();
+        calculateSimilarityScore();
+    }
+
+    // Part 1
+    public void calculateDistanceBetweenLists() {
+        int[] distances = new int[leftList.length];
+        for (int i = 0; i < leftList.length; i++) {
+            distances[i] = Math.abs(leftList[i] - rightList[i]);
+        }
+
+        int distanceBetweenLists = 0;
+        for (int distance : distances) {
+            distanceBetweenLists += distance;
+        }
+        System.out.printf("- Part 1: %d".indent(1), distanceBetweenLists);
+    }
+    // Part 2
+    public void calculateSimilarityScore() {
+        int similarityScore = 0;
+
+        for (int leftNumber : leftList) {
+            int foundInRightList = 0;
+
+            for (int rightNumber : rightList) {
+                if (leftNumber == rightNumber) foundInRightList++;
+            }
+
+            similarityScore += foundInRightList * leftNumber;
+        }
+
+        System.out.printf("- Part 2: %d".indent(1), similarityScore);
+    }
+
+    public String readFile(String fileName) {
+        try (var in = HistorianHysteria.class.getResourceAsStream("/%s".formatted(fileName))) {
+            assert in != null;
+            return new String(in.readAllBytes(), StandardCharsets.UTF_8);
+        }
+        catch (IOException | NullPointerException e) {
+            logger.error("An Exception occurred while trying to readFile() with name '{}': {}", fileName, e.getMessage());
+            return null;
+        }
+    }
+    public ArrayList<int[]> splitFileData(String fileData) {
+        String[] splittedNumber = fileData.split("\\s+");
+
+        // halbe Länge, weil links und rechts jeweils die Hälfte bekommen
+        int[] leftList = new int[splittedNumber.length / 2];
+        int[] rightList = new int[splittedNumber.length / 2];
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        for (int i = 0; i < splittedNumber.length; i++) {
+            if (i % 2 == 0) { // even index -> left
+                leftList[leftIndex++] = Integer.parseInt(splittedNumber[i]);
+            } else {          // odd index -> right
+                rightList[rightIndex++] = Integer.parseInt(splittedNumber[i]);
+            }
+        }
+
+        return new ArrayList<>(Arrays.asList(leftList, rightList));
+    }
+}
